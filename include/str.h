e157f6e98e020c01b0370a8adb653a3ce09c599b
xtco3o <xtco3o@gmail.com>
2026-06-04 06:23:04 +0800

    .

--- include/str.h
+++ include/str.h
@@ -10,26 +10,22 @@
 static inline bool IsEq(butil::StringPiece s1, std::string_view s2)
 {
     const size_t len = s2.size();
-    return s1.size() == len &&
-           strncasecmp(s1.data(), s2.data(), len) == 0;
+    return s1.size() == len && strncasecmp(s1.data(), s2.data(), len) == 0;
 }
 
 static inline bool IsEq(std::string_view s1, std::string_view s2)
 {
     const size_t len = s2.size();
-    return s1.size() == len &&
-           strncasecmp(s1.data(), s2.data(), len) == 0;
+    return s1.size() == len && strncasecmp(s1.data(), s2.data(), len) == 0;
 }
 
-static inline bool IsEq(const std::string& s1, std::string_view s2)
+static inline bool IsEq(const std::string &s1, std::string_view s2)
 {
     const size_t len = s2.size();
-    return s1.size() == len &&
-           strncasecmp(s1.data(), s2.data(), len) == 0;
+    return s1.size() == len && strncasecmp(s1.data(), s2.data(), len) == 0;
 }
 
-
-static inline bool IsEq(const char* s1, std::string_view s2)
+static inline bool IsEq(const char *s1, std::string_view s2)
 {
     return IsEq(std::string_view(s1), s2);
 }
@@ -47,13 +43,13 @@ static inline bool IsEqAny(std::string_view s1, Args &&...args)
 }
 
 template <typename... Args>
-static inline bool IsEqAny(const std::string& s1, Args &&...args)
+static inline bool IsEqAny(const std::string &s1, Args &&...args)
 {
     return (IsEq(s1, std::forward<Args>(args)) || ...);
 }
 
 template <typename... Args>
-static inline bool IsEqAny(const char* s1, Args &&...args)
+static inline bool IsEqAny(const char *s1, Args &&...args)
 {
     return (IsEq(s1, std::forward<Args>(args)) || ...);
 }
